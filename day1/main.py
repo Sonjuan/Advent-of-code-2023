36ccce9255e740cb2311056f59292a93e8a93166
Sonjuan <simset1212@gmail.com>
2023-12-03 14:03:09 +0900

    solved day1

---
+++ day1/main.py
@@ -0,0 +1,31 @@
+# part 1
+# sum = 0
+
+# for line in open(0):
+#     print(line)
+#     v = ''
+#     for char in line:
+#         if char.isdigit() == True :
+#             v += char
+#     v = v[0] + v[-1]
+#     sum += int(v)
+
+# print(sum)
+
+sum = 0
+
+for line in open(0):
+    line = line.rstrip()
+    v = ''
+    for i, char in enumerate(line):
+        if char.isdigit() == True :
+            v += char
+        else :
+            for j, val in enumerate(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']) :
+                if line[i:].startswith(val) :
+                    v += str((j+1))
+    v = v[0] + v[-1]
+    sum += int(v)
+
+print(sum)
+
