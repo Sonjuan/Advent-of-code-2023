c1356898fc0483bed2bec86d7a500866f4a24031
Sonjuan <simset1212@gmail.com>
2023-12-24 13:55:39 +0900

    revisit day21p1

--- day21/day21p1.py
+++ day21/day21p1.py
@@ -4,19 +4,14 @@ file = open(0).read().splitlines()
 
 N = len(file)
 M = len(file[0])
-sr = 0
-sc = 0
 
-for i, line in enumerate(file) :
-    for j, ch in enumerate(line) :
-        if ch == 'S' :
-            sr, sc = i, j
+sr, sc = next( (r,c) for r, row in enumerate(file) for c, ch in enumerate(row) if ch =='S'  )
 
 dr = [0,0,1,-1]
 dc = [-1,1,0,0]
 
-cache = [(sr,sc,0)]
-vis = [(sr,sc)]
+ans = []
+vis = {(sr,sc)}
 
 q = deque()
 q.append((sr,sc,0))
@@ -24,24 +19,23 @@ q.append((sr,sc,0))
 while q :
     R, C, step = q.popleft()
 
+    if step > 64 :
+        continue
+
+    if step % 2 == 0 :
+        ans.append((R,C))    
+    
     for k in range(4) :
-        nr = R + dr[k]
-        nc = C + dc[k]
+        nr, nc = R + dr[k], C + dc[k]
         if nr < 0 or nc < 0 or nr >= N or nc >= M :
             continue
+
         if file[nr][nc] == '#' :
             continue
+
         if (nr,nc) in vis :
             continue
-        if step+1 > 64 :
-            continue
-        vis.append((nr,nc))
-        cache.append((nr,nc,step+1))
-        q.append((nr,nc,step+1))
 
-ans = 0
-for i in cache :
-    r,c,step = i
-    if step % 2 == 0 :
-        ans += 1
-print(ans)
+        vis.add((nr,nc))
+        q.append((nr,nc,step+1))
+print(len(ans))
